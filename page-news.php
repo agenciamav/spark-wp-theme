fc02b7b06fb68fdf7f16d38baa2ef603ad473655
Luciano Tonet <tonet@gmail.com>
2016-04-28 22:28:41 -0300

    Corrigido CTA página news. Fixes #17

--- page-news.php
+++ page-news.php
@@ -165,37 +165,6 @@ Global Page Section Start
 				</div>
 			</section>
 
-	<?php if(rwmb_meta( 'spark_cta_post')){ ?>
-	<!--
-	==================================================
-	Call To Action Section Start
-	================================================== -->
-	<section id="call-to-action">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="block">
-						<?php
-                                   
-                            $cta = get_post( rwmb_meta( 'spark_cta_post') ); 
-                            if( $cta ) {
-                                $title      = $cta->post_title;
-                                $content    = $cta->post_content;                                
-                        ?>
-                                    
-                                <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="300ms"><?php echo $title; ?></h1>
-                                <?php echo $content; ?>
-                                                                    
-                        <?php  } ?>  
-						<h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">PREPARADO PARA EXPANDIR O SEU NEGÓCIO?</h1>
-							<p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Texto texto texto texto texto texto texto texto texto texto texto<br> texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto.</p>
-							<a href="#" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Solicite contato</a> <a href="#" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".8s" data-wow-duration="500ms">Fale com um Consultor</a>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</section>
-	<?php  } ?>
+	<?php get_template_part('section', 'calltoaction' ); ?> 
 
 <?php get_footer();?>
